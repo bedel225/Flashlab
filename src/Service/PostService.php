5c48b128cab311642b19143388d4178a52a2499f
afofana <afofana@flashlab.fr>
2025-06-26 15:52:27 +0200

    FA optim

--- src/Service/PostService.php
+++ src/Service/PostService.php
@@ -1,35 +1,31 @@
 <?php
 
 namespace App\Service;
+
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class PostService
 {
     private $client;
+
     public function __construct(HttpClientInterface $client)
     {
-        $this->client = $client;
-        
+        $this->client = $client;        
     }
 
-   /* public function saveData()
-    {
-        for($)
-    }*/
-    public function fetchPostInformation(): array
+   public function fetchPostInformation(): array
     {
         $response = $this->client->request(
             'GET',
             'https://jsonplaceholder.typicode.com/posts'
         );
+
         $statusCode = $response->getStatusCode();
-        // $statusCode = 200
-        $contentType = $response->getHeaders()['content-type'][0];
-        // $contentType = 'application/json'
-        $content = $response->getContent();
-        // $content = '{"id":521583, "name":"symfony-docs", ...}'
-        $content = $response->toArray();
-        // $content = ['id' => 521583, 'name' => 'symfony-docs', ...]
-        return $content;
+        if ($statusCode !== 200) {
+            throw new \Exception("Erreur API : code $statusCode");
+        }
+
+        return $response->toArray();
     }
+
 }

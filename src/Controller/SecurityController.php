e22483665d8ce0f3dc106530e3d934b17dbab1a2
bedel225 <abdoulchoco@yahoo.fr>
2025-07-09 03:34:07 +0200

    securityController

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,14 +5,28 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
-    #[Route('/security', name: 'app_security')]
-    public function index(): Response
+    #[Route('/login', name: 'login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('security/index.html.twig', [
-            'controller_name' => 'SecurityController',
+        // ✅ On récupère l'erreur éventuelle
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // ✅ On récupère le dernier email saisi
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        // ✅ On passe ces variables au template
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
         ]);
+    }
+
+    #[Route('/logout', name: 'logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
